fab122838540ab33eba412075458701185936f0c
Hyuk <hyukho83@gmail.com>
2025-01-11 05:19:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# OX Quiz
+
+oxquiz2/ContentView.swift에 코드작성이 되었있습니다.
+
+기초 산수 퀴즈로 문제를 생성하는 것이 특징입니다.
---
+++ oxquiz2/oxquiz2App.swift
@@ -0,0 +1,17 @@
+//
+//  oxquiz2App.swift
+//  oxquiz2
+//
+//  Created by Hyuk Ho Song on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct oxquiz2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
